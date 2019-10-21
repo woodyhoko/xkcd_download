cfc3d660818df480573cafb2f208dd21591dd3fb
woodyhoko <woodyhoko@gmail.com>
2019-10-21 21:04:15 +0800

    Add files via upload

--- download.py
+++ download.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 # 1037 no comic
 # 1331 special case
 # 2067 special case
-
+try os.mkdir("data")
 with open("details.txt", "a") as f:
 	for i in range(2067,2218):
 		url = "http://xkcd.com/"+str(i)
@@ -20,14 +20,14 @@ with open("details.txt", "a") as f:
 		try :
 			link = img.contents[5].img['srcset'][:-2]
 			print '\n', i, '\t', link
-			wget.download("http:"+link,out=str(i)+"_"+link.split('/')[-1].replace("_2x",""))
+			wget.download("http:"+link,out="./data/"+str(i)+"_"+link.split('/')[-1].replace("_2x",""))
 		except :
 			if len(img.contents[12])>2:
 				link = img.contents[12].split(':')[-1]
 			else :
 				link = img.contents[5].img['src']
 			print '\n', i, '\t', link
-			wget.download("http:"+link,out=str(i)+"_"+link.split('/')[-1].replace("_2x",""))
+			wget.download("http:"+link,out="./data/"+str(i)+"_"+link.split('/')[-1].replace("_2x",""))
 		f.write("number : "+str(i)+"\n")
 		f.write("title : "+link.split('/')[-1].replace("_2x",""))		
 		f.write("link : http:"+link)
